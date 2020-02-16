7e19470f8b2f8d840fa1a3c3d8053feb69619f22
wordaddict <madeyinka6@gmail.com>
2020-02-16 16:07:20 +0100

    reverse a queue of k list using a stack

--- queue.js
+++ queue.js
@@ -1,3 +1,5 @@
+const Stack = require('./stack')
+
 class Queue {
     constructor(){
         this.queue = [];
@@ -7,6 +9,11 @@ class Queue {
         this.queue.push(item);
         return this.queue;
     };
+
+    // Adds to the top of the queue
+    front(){
+        return this.queue[0]
+    }
 
     dequeue(){
         if(this.queue.length === 0){
@@ -25,13 +32,44 @@ class Queue {
     list(){
         return this.queue;
     }
+
+    size(){
+        return this.queue.length;
+    }
 }
 
 const q = new Queue();
-q.enqueue(5)
-q.enqueue(6)
-q.enqueue(7)
+q.enqueue(10)
+q.enqueue(20)
+q.enqueue(30)
+q.enqueue(40)
+q.enqueue(50)
+q.enqueue(60)
 
 console.log('list', q.list())
 console.log('peak', q.peek())
 console.log('list 2', q.list())
+
+const reverse = (q, k) => {
+    const s = new Stack();
+    for (let i = 0; i < k; i++){
+     
+        s.push(q.peek())
+        q.dequeue()
+    }
+
+
+    while(!s.empty()){
+        q.enqueue(s.top())
+        s.pop()
+    }
+
+    for(let i = 0; i < q.size() - k; i++){
+        q.enqueue(q.front())
+        q.dequeue()
+    }
+    return q;
+}
+
+const data = [10, 20, 30, 40, 50, 60];
+console.log('solution', reverse(q, 4));
--- stack.js
+++ stack.js
@@ -22,17 +22,31 @@ class Stack {
         return this.stack[this.stack.length - 1]
     }
 
+    // back(){
+    //     return this.stack[0]
+    // }
+
+    empty(){
+        return this.stack.length === 0 ? true : false;
+    }
+
     list(){
         return this.stack;
     }
+
+    size(){
+        return this.stack.length;
+    }
 }
 
-const s = new Stack();
-s.push(5)
-s.push(6)
-s.push(7)
+module.exports = Stack;
 
-console.log('list', s.list())
-console.log('pop', s.pop())
-console.log('peak', s.top())
-console.log('list 2', s.list())
+// const s = new Stack();
+// s.push(5)
+// s.push(6)
+// s.push(7)
+
+// console.log('list', s.list())
+// console.log('pop', s.pop())
+// console.log('peak', s.top())
+// console.log('list 2', s.list())

5ebd9a0e3c3be17136b41c6875a6642cc0ce33d3
wordaddict <madeyinka6@gmail.com>
2020-02-24 01:01:59 +0100

    concatenated words

---
+++ concatenatedwords.js
@@ -0,0 +1,42 @@
+/**
+ * @param {string[]} words
+ * @return {string[]}
+ */
+// var findAllConcatenatedWordsInADict = function(words) {
+    
+// };
+
+function isComposedBy(word, set){
+    let arr = new Array(word.length + 1).fill(false);  
+    arr[0] = true;  
+    set.delete(word);
+    
+    for(let i = 1; i <= word.length + 1; i++){    
+      let checkpoints = arr.reduce((points, curr, i) => {
+        if (curr) points.push(i);
+        return points;
+      }, []);
+      let snippets = checkpoints.map(cp => word.substring(cp, i));        
+      for (let snippet of snippets){
+        if (set.has(snippet)){      
+          arr[i] = true;
+          break;
+        }   
+      }          
+      
+    }  
+    set.add(word);  
+    return arr[arr.length - 1];
+  }
+  
+  var findAllConcatenatedWordsInADict = function(words) {  
+    if (words.length < 2) return [];
+    let wordSet = new Set(words);  
+    if (wordSet.has("")) wordSet.delete("");
+    let result = [];
+    words.forEach(word => {
+      if (word.length > 0 && isComposedBy(word, wordSet)) 
+        result.push(word);
+    });
+    return result;
+  };
